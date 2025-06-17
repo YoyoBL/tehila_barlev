669ea7b6af71eb9a69cdc002e1eb75376bb78b83
YoyoBLemail <yoelbl2@gmail.com>
2025-06-17 20:32:50 +0300

    Updated Cover image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import { pinyon, playFairDisplay } from "@/lib/fonts";
 import cover from "@/images/Cover.jpg";
-import Image from "next/image";
 import Link from "next/link";
 import { ROUTES } from "../lib/constants";
 import { METADATA } from "@/lib/texts";
@@ -10,21 +9,21 @@ export const metadata = METADATA.homePage;
 function App() {
    return (
       <section
-         className="h-full"
+         className="h-full flex flex-col"
          style={{
             backgroundImage: `url(${cover.src})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
          }}
       >
-         <div className="glass fixed bottom-0 h-[200px] w-full flex flex-col gap-3 items-center justify-center bg-base-100 rounded-t-box">
+         <div className="glass mt-auto h-[200px] w-full flex flex-col gap-3 items-center justify-center bg-base-100 rounded-t-box">
             <h2 className={`${playFairDisplay.className} text-4xl`}>
                New Collection
             </h2>
             <h2 className={`${pinyon.className}`}>2025</h2>
             <Link
                href={ROUTES.catalog.path}
-               className="btn btn-primary w-[250px] h-16  font-light text-3xl"
+               className="btn btn-primary w-[250px] h-16 font-light text-3xl"
             >
                לשמלות
             </Link>
